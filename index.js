dbd0297ebc48732b804633b4dfdcb1f8828fd988
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-03-16 18:04:42 +0000

    using .get from heroku tutorial

--- index.js
+++ index.js
@@ -33,6 +33,20 @@ const addVerb = (request, response) => {
   )
 }
 
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM germanverbs');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+
 app.route('/verbs').get('/verbs', getVerbs).post('addVerb)
 app.route('/').get('/', function (req, res) { res.send('Hello'); });
 
