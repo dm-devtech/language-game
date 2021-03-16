dc7f7c2511647e4e4e530fe956fb7bbc3cabd398
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-03-16 17:07:09 +0000

    reverted index.js to use bodyparser

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const {pool} = require('./config')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 
 const getVerbs = (request, response) => {
