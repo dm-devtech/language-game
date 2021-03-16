01a7568a9da17a539189791e5e06cb777e9d064e
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-03-16 19:23:57 +0000

    using test connection on home route

--- index.js
+++ index.js
@@ -1,49 +1,78 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const {pool} = require('./config')
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const {pool} = require('./config')
+//
+// const app = express()
+//
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// app.use(express.static("public"))
+// app.use(cors())
+//
+// const getVerbs = (request, response) => {
+//   pool.query('SELECT * FROM germanverbs', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results)
+//   })
+// }
+//
+// const addVerb = (request, response) => {
+//   const {engverb, gerverb} = request.body
+//   pool.query(
+//     'INSERT INTO germanverbs (engverb, gerverb) VALUES ($1, $2)',
+//     [engverb, gerverb],
+//     (error) => {
+//       if (error) {
+//         throw error
+//       }
+//       response.status(201).json({status: 'success', message: 'Verb added.'})
+//     },
+//   )
+// }
+//
+// app.route('/verbs').get(getVerbs)
+// app.get('/', function (req, res) { res.send('Hello'); });
+//
+// app.get('/test', function(req, res) {
+//   pool.query("SELECT * FROM germanverbs", function(error, result){
+//     const sth = res.json(result);
+//     console.log(sth)
+//   });
+// });
+//
+// // Start server
+// app.listen(process.env.PORT || 3002, () => {
+//   console.log(`Server listening`)
+// })
 
-const app = express()
+const { Pool } = require('pg');
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static("public"))
-app.use(cors())
+const env = process.env.NODE_ENV || 'development';
 
-const getVerbs = (request, response) => {
-  pool.query('SELECT * FROM germanverbs', (error, results) => {
+let connectionString = {
+    user: secrets.user,
+    database: secrets.testDb,
+    host: secrets.host
+};
+// checking to know the environment and suitable connection string to use
+if (env === 'development') {
+    console.log("development")
+} else {
+    connectionString = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+    };
+};
+const pool = new Pool(connectionString);
+pool.on('connect', () => console.log('connected to db'));
+const data = pool.query('SELECT * FROM germanverbs', (error, results) => {
     if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-const addVerb = (request, response) => {
-  const {engverb, gerverb} = request.body
-  pool.query(
-    'INSERT INTO germanverbs (engverb, gerverb) VALUES ($1, $2)',
-    [engverb, gerverb],
-    (error) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).json({status: 'success', message: 'Verb added.'})
-    },
-  )
-}
-
-app.route('/verbs').get(getVerbs)
-app.get('/', function (req, res) { res.send('Hello'); });
-
-app.get('/test', function(req, res) {
-  pool.query("SELECT * FROM germanverbs", function(error, result){
-    const sth = res.json(result);
-    console.log(sth)
-  });
-});
-
-// Start server
-app.listen(process.env.PORT || 3002, () => {
-  console.log(`Server listening`)
-})
+       throw error
+     }
+     response.status(200).json(results)
+    })
+    
+app.get('/', function (req, res) { res.send(data); });
