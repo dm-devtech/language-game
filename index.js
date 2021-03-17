ba36d3ed2d66d6f8a2f34fb24e0af976afbbe1e7
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-03-17 16:43:49 +0000

    using example from heroku documentation for connecting in node

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const express = require('express')
 const cors = require('cors')
 // const {pool} = require('./config')
 //
-const { Pool } = require('pg');
-const app = express()
-
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static("public"))
-app.use(cors())
+const { Client } = require('pg');
+// const app = express()
+//
+//
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// app.use(express.static("public"))
+// app.use(cors())
 //
 // const getVerbs = (request, response) => {
 //   pool.query('SELECT * FROM germanverbs', (error, results) => {
@@ -46,30 +46,51 @@ app.use(cors())
 // });
 //
 // // Start server
+//
+//
+// const env = process.env.NODE_ENV
+// console.log("ENV>>>>>>>>>>>>", env)
+// if (env === 'production') {
+//     console.log("STATUS>>>>>>>>>>>>>", "production")
+//     connectionString = {
+//       connectionString: process.env.DATABASE_URL,
+//       ssl: {
+//         rejectUnauthorized: false
+//       }
+//     }
+// }
+//
+// const pool = new Pool(connectionString);
+// console.log("pool>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", pool)
+//
+// const data = pool.query('SELECT * from germanverbs')
+// .then(res => res.rows)
+// .catch(err => console.error('Error executing query', err.stack))
+// console.log(data)
+//
+// app.get('/', function (req, res) { res.send(data); });
+//
+// app.listen(process.env.PORT || 3002, () => {
+//   console.log("server listening>>>>>>>>>>", `Server listening`)
+// })
 
 
-const env = process.env.NODE_ENV
-console.log("ENV>>>>>>>>>>>>", env)
-if (env === 'production') {
-    console.log("STATUS>>>>>>>>>>>>>", "production")
-    connectionString = {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
-}
 
-const pool = new Pool(connectionString);
-console.log("pool>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", pool)
+const { Client } = require('pg');
 
-const data = pool.query('SELECT * from germanverbs')
-.then(res => res.rows)
-.catch(err => console.error('Error executing query', err.stack))
-console.log(data)
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-app.get('/', function (req, res) { res.send(data); });
+client.connect();
 
-app.listen(process.env.PORT || 3002, () => {
-  console.log("server listening>>>>>>>>>>", `Server listening`)
-})
+client.query('SELECT * FROM germanverbs', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
