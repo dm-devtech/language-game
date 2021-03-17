ff9838ba4cc44f29d6411d70bdb3b1ffc72c33ba
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-03-17 17:50:48 +0000

    updated code api works on heroku, renamed table columns too

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE languagegame;
-CREATE TABLE germanverbs (ID SERIAL PRIMARY KEY, engverb VARCHAR(100) NOT NULL, gerverb VARCHAR(100) NOT NULL);
+CREATE TABLE germanverbs (ID SERIAL PRIMARY KEY, "verb-en" VARCHAR(100) NOT NULL, "verb-de" VARCHAR(100) NOT NULL);
 INSERT INTO germanverbs (engverb, gerverb) VALUES ('To run', 'laufen');
 
 
-CREATE TABLE germannouns (ID SERIAL PRIMARY KEY, engnoun VARCHAR(100) NOT NULL, gernoun VARCHAR(100) NOT NULL);
+CREATE TABLE germannouns (ID SERIAL PRIMARY KEY, "noun-en" VARCHAR(100) NOT NULL, "noun-de" VARCHAR(100) NOT NULL);
 INSERT INTO germannouns (engnoun, gernoun) VALUES ('pan', 'die Pfanne');
