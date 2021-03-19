53e2591b61e7084fe571814b586aaa5890fc8987
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-03-19 16:20:52 +0000

    updated db schema

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE languagegame WITH ENCODING 'UTF8';
-CREATE TABLE german (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "ger" VARCHAR(100) NOT NULL, "word_type" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL);
-INSERT INTO german (eng, ger, "word_type", category) VALUES ('To run', 'laufen', 'verb', 'movement');
+CREATE TABLE german (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "ger" VARCHAR(100) NOT NULL, "word_type" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL, "gender" VARCHAR(100) NOT NULL);
+INSERT INTO german (eng, ger, "word_type", category) VALUES ('To run', 'laufen', 'verb', 'movement', 'n/a');
 
-CREATE TABLE french (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "fre" VARCHAR(100) NOT NULL, "word_type" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL);
-INSERT INTO french (eng, fre, "word_type", category) VALUES ('pan', 'la poêle', 'noun', 'kitchen');
+CREATE TABLE french (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "fre" VARCHAR(100) NOT NULL, "word_type" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL, "gender" VARCHAR(100) NOT NULL);
+INSERT INTO french (eng, fre, "word_type", category) VALUES ('pan', 'la poêle', 'noun', 'kitchen', 'la');
 
-CREATE TABLE latin (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "lat" VARCHAR(100) NOT NULL, "word_type" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL);
-INSERT INTO latin (eng, lat, "word_type", category) VALUES ('pan', 'cacabus', 'noun', 'kitchen');
+CREATE TABLE latin (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "lat" VARCHAR(100) NOT NULL, "word_type" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL, "gender" VARCHAR(100) NOT NULL);
+INSERT INTO latin (eng, lat, "word_type", category) VALUES ('pan', 'cacabus', 'noun', 'kitchen', 'masculine');
