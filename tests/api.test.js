1d0812fda572ca341962c37b4fcf707d1f05aef1
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-03-22 16:21:48 +0000

    added tests for each local server language dictionary api

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,4 +8,22 @@ describe('testing the local page', () => {
     expect(homeResponse.text).toContain('Language Lighthouse')
     done()
   })
+
+  it('Test german dictionary api', async done => {
+    const homeResponse = await request.get('/german')
+    expect(homeResponse.text).toContain('[{"id":1,"eng":"To run","ger":"laufen","wordtype":"verb","category":"movement","gender":"n/a"}]')
+    done()
+  })
+
+  it('Test french dictionary api', async done => {
+    const homeResponse = await request.get('/french')
+    expect(homeResponse.text).toContain('[{"id":1,"eng":"pan","fre":"la poêle","wordtype":"noun","category":"kitchen","gender":"la"}]')
+    done()
+  })
+
+  it('Test latin dictionary api', async done => {
+    const homeResponse = await request.get('/latin')
+    expect(homeResponse.text).toContain('[{"id":1,"eng":"pan","lat":"cacabus","wordtype":"noun","category":"kitchen","gender":"masculine"}]')
+    done()
+  })
 })
