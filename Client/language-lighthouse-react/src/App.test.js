f75ad68406c33e2bb0f1e4fc950f71d4b328c512
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-03 14:05:30 +0100

    added logic to pick three words including correct and incorrect words

--- Client/language-lighthouse-react/src/App.test.js
+++ Client/language-lighthouse-react/src/App.test.js
@@ -1,28 +1,59 @@
 import App from './App';
-import { render, fireEvent, cleanup, screen } from '@testing-library/react';
+import { fireEvent, cleanup, screen } from '@testing-library/react';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import Controls from "./components/Controls"
 
-global.fetch = jest.fn(() => Promise.resolve({
-  json: () => Promise.resolve({
-    eng: "to run", ger: "laufen",
-  }),
+let container = null
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 })
-)
 
-afterEach(cleanup)
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+})
 
 describe('Homepage <Controls />', () => {
   it('renders without crashing', () => {
-    render(<App />);
+    render(<Controls id="123" />, container)
   });
-  it('test Title is shown works', () => {
-     const {getByText} = render(<App />);
-     getByText(/Language LightHouse/);
-  })
+  // it('test Title is shown works', () => {
+  //    render(<Controls />, container)
+  //    expect(container.querySelector("h1").textContent).toBe("Language LightHouse")
+  // })
 })
 
 describe('App', () => {
-  it('test Title is shown works', async () => {
-     await act(async () => render(<App />));
-     expect(screen.getByText("laufen")).toBeInDocument()
+  it('renders data', async () => {
+
+//     const fakeWord = {
+//       id: 0,
+//       eng: 'to run',
+//       ger: 'laufen'
+//     };
+//     jest.spyOn(global, "fetch").mockImplementation(() =>
+//       Promise.resolve({
+//         status: 200,
+//         json: () => Promise.resolve(fakeWord)
+//       }))
+//
+//     // jest.spyOn(global, 'fetch').mockResolvedValue({ json: jest.fn().mockResolvedValue({ fakeWord }) });
+//
+// // use asynchronous version of act to apply resolved Promise
+  await act(async () => {
+    render(<Controls />, container)
+    const test = await screen.queryByTestId('eng')
+    console.log(test)
+  })
+
+  expect(await screen.queryByTestId('eng').textContent).toBe("english:")
+  expect(await screen.queryByTestId('ger').textContent).toBe("german:")
+  // global.fetch.mockRestore();
   })
 })
--- Client/language-lighthouse-react/src/components/Controls.js
+++ Client/language-lighthouse-react/src/components/Controls.js
@@ -6,33 +6,78 @@ class Controls extends Component {
     super(props)
       this.state = {
         wordToMatch: [],
-        allWords: []
+        selectionOne: [],
+        selectionTwo: [],
+        selectionThree: []
     }
+  }
+
+  myRandomInts(quantity, max){
+    const set = new Set()
+    while(set.size < quantity) {
+      set.add(Math.floor(Math.random() * max) + 1)
+    }
+    return set
   }
 
   async componentDidMount() {
     const url = 'http://language-lighthouse.herokuapp.com/api/german'
     const response = await fetch(url)
     const data = await response.json()
-    this.setState({allWords: data, loading: false})
-    this.setState({wordToMatch: data[Math.floor(Math.random() * this.state.allWords.length)], loading: false})
+
+    const incorrectSelections = this.myRandomInts(2, data.length) // object of two random words that are different to the word
+    const correctSelection = Math.floor(Math.random() * data.length) // the random number used to find a random word as basis of turn
+    const values = Array.from(incorrectSelections, (v, i) => v-1) // converting object of two incorrect words into an array and zero indexing
+    const allWords = [] // create empty array to push the main word and two incorrect words into
+    allWords.push(correctSelection, values[0], values[1]) // pushing correct word and two incorrect words into this array
+    const randomOrder = this.myRandomInts(3, allWords.length) // generating three random numbers to use to randomise order of allWords
+    const order = Array.from(randomOrder, (v, i) => v-1) // converting order obj to array and zero indexing
+    const randomSelectionOne = allWords[order[0]] // random number for selection 1 using array of allWords and using random order
+    const randomSelectionTwo = allWords[order[1]] // random number for selection 2 using array of allWords and using random order
+    const randomSelectionThree = allWords[order[2]] // random number for selection 3 using array of allWords and using random order
+
+    this.setState({wordToMatch: data[correctSelection], loading: false})
+    this.setState({selectionOne: data[randomSelectionOne], loading: false})
+    this.setState({selectionTwo: data[randomSelectionTwo], loading: false})
+    this.setState({selectionThree: data[randomSelectionThree], loading: false})
   }
 
   render() {
     return (
-      <div >
+      <div>
         <h1>Language LightHouse</h1>
           {this.state.loading || !this.state.wordToMatch ? (
             <div>loading...</div>
           ) : (
-              <div>
-              English: {this.state.wordToMatch.eng} <br/>
-              German: {this.state.wordToMatch.ger}
-              </div>
+          <div>
+                <div data-testid="eng">english: {this.state.wordToMatch.eng}</div>
+                <div data-testid="ger">german: {this.state.wordToMatch.ger}</div>
+                <br/>
+                Select from the below:
+                <br/>
+                <div>Num match: {this.state.selectionOne.ger}</div>
+                <div>num wrong: {this.state.selectionTwo.ger}</div>
+                <div>num wrong2: {this.state.selectionThree.ger}</div>
+          </div>
           )}
       </div>
     )
   }
 }
+
+// render() {
+//   return (
+//
+//         {this.state.loading || !this.state.allWords ? (
+//           <div>loading...</div>
+//         ) : (
+//             <div data-testid="eng">
+//             info: {this.state.allWords.eng}
+//
+//             </div>
+//           )
+// }
+// }
+
 
 export default Controls
