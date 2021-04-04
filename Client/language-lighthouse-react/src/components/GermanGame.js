199508d0deb0cb2d0c6ceeeb47f0aeee1a9f91c9
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-04 14:07:33 +0100

    added check answer logic

--- Client/language-lighthouse-react/src/components/GermanGame.js
+++ Client/language-lighthouse-react/src/components/GermanGame.js
@@ -8,7 +8,9 @@ class Controls extends Component {
         wordToMatch: [],
         selectionOne: [],
         selectionTwo: [],
-        selectionThree: []
+        selectionThree: [],
+        counter: 0,
+        answer: 0
     }
   }
 
@@ -29,6 +31,13 @@ class Controls extends Component {
     return set
   }
 
+  checkAnswer = e => {
+    let id = parseInt(e.target.getAttribute('word_id'))
+    id === this.state.wordToMatch.id ? console.log("correct") : console.log("incorrect")
+    // id === this.state.wordToMatch.id ? console.log("correct") : console.log("incorrect")
+    // this.setState({counter: this.state.counter + 1})
+  }
+
   async componentDidMount() {
     const url = 'http://language-lighthouse.herokuapp.com/api/german'
     const response = await fetch(url)
@@ -45,32 +54,19 @@ class Controls extends Component {
 
     const availableSelectionsObj = this.myRandomInts(3, apiLength, correctSelection) // object of selections available
     const availableSelectionsArr = Array.from(availableSelectionsObj, (v, i) => v) // converting object of available selections into an array and zero indexing
-    console.log("correctSelection")
-    console.log(correctSelection)
-
-    console.log("apiLength", "correctSelection", "availableSelectionsObj", "availableSelectionsArr")
-    console.log(apiLength, correctSelection, availableSelectionsObj, availableSelectionsArr)
 
     const randomOrderObj = this.myRandomOrder(3, 3) // PROBLEM AREA generating three random numbers to use to randomise order of available selections
     const orderArr = Array.from(randomOrderObj, (v, i) => v) // converting order obj to array and zero indexing
-
-    console.log("orderArr", "randomOrderObj")
-    console.log(orderArr, randomOrderObj)
 
     const randomSelectionOne = availableSelectionsArr[orderArr[0]-1] // random number for selection 1 using array of allWords and using random order
     const randomSelectionTwo = availableSelectionsArr[orderArr[1]-1] // random number for selection 2 using array of allWords and using random order
     const randomSelectionThree = availableSelectionsArr[orderArr[2]-1] // random number for selection 3 using array of allWords and using random order
 
-    console.log("orderArr[0]", "orderArr[1]", "orderArr[2]")
-    console.log(orderArr[0], orderArr[1], orderArr[2])
-
-    console.log("randomSelectionOne", "randomSelectionTwo", "randomSelectionThree")
-    console.log(randomSelectionOne, randomSelectionTwo, randomSelectionThree)
-
     this.setState({wordToMatch: data[correctSelection], loading: false})
     this.setState({selectionOne: data[randomSelectionOne], loading: false})
     this.setState({selectionTwo: data[randomSelectionTwo], loading: false})
     this.setState({selectionThree: data[randomSelectionThree], loading: false})
+    this.setState({answer: 0, loading: false})
   }
 
   render() {
@@ -86,11 +82,13 @@ class Controls extends Component {
                 <br/>
                 Select from the below:
                 <br/>
-                <button>Option 1: {this.state.selectionOne.ger} </button>
+                <button onClick={this.checkAnswer} word_id={this.state.selectionOne.id}>Option 1: {this.state.selectionOne.ger} </button>
                 <br/>
-                <button>Option 2: {this.state.selectionTwo.ger} </button>
+                <button onClick={this.checkAnswer} word_id={this.state.selectionTwo.id}>Option 2: {this.state.selectionTwo.ger} </button>
                 <br/>
-                <button>Option 3: {this.state.selectionThree.ger} </button>
+                <button onClick={this.checkAnswer} word_id={this.state.selectionThree.id}>Option 3: {this.state.selectionThree.ger} </button>
+                <br/>
+                <button>Submit</button>
             </>
           </div>
           )}
