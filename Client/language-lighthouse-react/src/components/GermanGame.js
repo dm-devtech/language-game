9b9569a3eca1cea169e1b4c553676dc1aa6b7835
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-05 14:28:58 +0100

    tidying up game logic

--- Client/language-lighthouse-react/src/components/GermanGame.js
+++ Client/language-lighthouse-react/src/components/GermanGame.js
@@ -54,30 +54,27 @@ class Controls extends Component {
   }
 
   async changeWord() {
-    this.setState({counter: this.state.counter + 1})
-    this.setState({answer: "CORRECT"})
-    const correctSelection = await Math.floor(Math.random() * (this.state.apiLength))
-    this.setState({correctSelection: correctSelection}) // the random number used to find a random word as basis of turn
-    const newRandomWord = await this.state.apiData[this.state.correctSelection]
-    this.setState({wordToMatch: newRandomWord})
-    console.log("check wordToMatch 1>>>>>", this.state.wordToMatch.eng, "correctSelection", this.state.apiData[this.state.correctSelection], "new word", newRandomWord)
+    this.setState({counter: this.state.counter + 1}) // score +1 when answer correct
+    this.setState({answer: "CORRECT"}) // correct message shown during game
 
-    const randomArray = await Array.from(this.myRandomInts(3, this.state.apiLength, this.state.correctSelection), (v, i) => v)
-    this.setState({availableSelectionsArr: randomArray}) // converting object of available selections into an array and zero indexing
+    const correctSelection = await Math.floor(Math.random() * (this.state.apiLength)) // random number based on api length
+    this.setState({correctSelection: correctSelection}) // random number for word index
 
-    this.setState({orderArr: Array.from(this.myRandomOrder(3, 3), (v, i) => v)}) // converting order obj to array and zero indexing
+    const newRandomWord = await this.state.apiData[this.state.correctSelection] // using index with api to set word for turn
+    this.setState({wordToMatch: newRandomWord}) // allocating word to state
 
-    this.setState({randomSelectionOne: this.state.availableSelectionsArr[this.state.orderArr[0]-1]}) // random number for selection 1 using array of allWords and using random order
-    this.setState({randomSelectionTwo: this.state.availableSelectionsArr[this.state.orderArr[1]-1]}) // random number for selection 2 using array of allWords and using random order
-    this.setState({randomSelectionThree: this.state.availableSelectionsArr[this.state.orderArr[2]-1]}) // random number for selection 3 using array of allWords and using random order
-    console.log("check", "check randomSelectionOne>>>", this.state.randomSelectionOne, "check randomSelectionTwo>>>", this.state.randomSelectionTwo, "check randomSelectionThree>>>>", this.state.randomSelectionThree, "check availableSelectionsArr>>>>", this.state.availableSelectionsArr)
-    console.log("check wordToMatch 2>>>>>>", this.state.wordToMatch.eng)
+    const randomArray = await Array.from(this.myRandomInts(3, this.state.apiLength, this.state.correctSelection), (v, i) => v) // random numbers for selections
+    this.setState({availableSelectionsArr: randomArray}) // allocating random numbers to state
 
-    this.setState({selectionOne: this.state.apiData[this.state.randomSelectionOne]})
-    this.setState({selectionTwo: this.state.apiData[this.state.randomSelectionTwo]})
-    this.setState({selectionThree: this.state.apiData[this.state.randomSelectionThree]})
-    console.log("check api data", this.state.apiData, "check SelectionOne>>>", this.state.apiData[this.state.randomSelectionOne], "check SelectionTwo>>>", this.state.apiData[this.state.randomSelectionTwo], "check SelectionThree>>>>", this.state.apiData[this.state.randomSelectionThree])
-    // console.log("check SelectionOne>>>", this.state.SelectionOne.ger, "check SelectionTwo>>>", this.state.SelectionTwo.ger, "check SelectionThree>>>>", this.state.SelectionThree.ger, "check availableSelectionsArr>>>>", this.state.availableSelectionsArr)
+    this.setState({orderArr: Array.from(this.myRandomOrder(3, 3), (v, i) => v)}) // allocating randomised selection order to state
+
+    this.setState({randomSelectionOne: this.state.availableSelectionsArr[this.state.orderArr[0]-1]}) // randomising order of selections
+    this.setState({randomSelectionTwo: this.state.availableSelectionsArr[this.state.orderArr[1]-1]}) // same as above
+    this.setState({randomSelectionThree: this.state.availableSelectionsArr[this.state.orderArr[2]-1]}) // same as above
+
+    this.setState({selectionOne: this.state.apiData[this.state.randomSelectionOne]}) // setting selection to state used for buttons
+    this.setState({selectionTwo: this.state.apiData[this.state.randomSelectionTwo]}) // same as above
+    this.setState({selectionThree: this.state.apiData[this.state.randomSelectionThree]}) // same as above
 
   }
 
@@ -86,7 +83,7 @@ class Controls extends Component {
     const url = 'http://language-lighthouse.herokuapp.com/api/german'
     const response = await fetch(url)
     const data = await response.json()
-    this.setState({apiData: data, loading: false})
+    this.setState({apiData: data, loading: false}) // setting data to state
 
     // const cat = "modal" // category
     // const newData = data.filter(function (word) { // filter array by category = modal verbs.  array indexes can then be used
@@ -95,27 +92,22 @@ class Controls extends Component {
     // console.log(newData)
 
     this.setState({apiLength: data.length-1})
-    console.log("dm api length", this.state.apiLength)
-    this.setState({correctSelection: Math.floor(Math.random() * (this.state.apiLength)) }) // the random number used to find a random word as basis of turn
-    this.setState({wordToMatch: this.state.apiData[this.state.correctSelection]})
-    console.log("did mount wordToMatch", this.state.wordToMatch.eng)
 
-    this.setState({randomArray: Array.from(this.myRandomInts(3, this.state.apiLength, this.state.correctSelection), (v, i) => v)})
-    this.setState({availableSelectionsArr: this.state.randomArray}) // converting object of available selections into an array and zero indexing
+    this.setState({correctSelection: Math.floor(Math.random() * (this.state.apiLength)) }) // random number based on api length
+    this.setState({wordToMatch: this.state.apiData[this.state.correctSelection]}) // using index with api to set word for turn
+
+    this.setState({randomArray: Array.from(this.myRandomInts(3, this.state.apiLength, this.state.correctSelection), (v, i) => v)}) // random numbers for selections
+    this.setState({availableSelectionsArr: this.state.randomArray}) // random numbers for selections allocated to available selections
 
     this.setState({orderArr: Array.from(this.myRandomOrder(3, 3), (v, i) => v)}) // converting order obj to array and zero indexing
-    console.log("did mount", "orderArr", this.state.orderArr)
 
     this.setState({randomSelectionOne: this.state.availableSelectionsArr[this.state.orderArr[0]-1]}) // random number for selection 1 using array of allWords and using random order
     this.setState({randomSelectionTwo: this.state.availableSelectionsArr[this.state.orderArr[1]-1]}) // random number for selection 2 using array of allWords and using random order
     this.setState({randomSelectionThree: this.state.availableSelectionsArr[this.state.orderArr[2]-1]}) // random number for selection 3 using array of allWords and using random order
-    console.log("DM randomSelectionOne>>>", this.state.randomSelectionOne, "dm randomSelectionTwo>>>", this.state.randomSelectionTwo, "DM randomSelectionThree>>>>", this.state.randomSelectionThree)
 
-    this.setState({selectionOne: this.state.apiData[this.state.randomSelectionOne]})
-    this.setState({selectionTwo: this.state.apiData[this.state.randomSelectionTwo]})
-    this.setState({selectionThree: this.state.apiData[this.state.randomSelectionThree]})
-    console.log("dm api data", this.state.apiData, "dm SelectionOne>>>", this.state.apiData[this.state.randomSelectionOne], "dm SelectionTwo>>>", this.state.apiData[this.state.randomSelectionTwo], "dm SelectionThree>>>>", this.state.apiData[this.state.randomSelectionThree])
-    // console.log("dm SelectionOne>>>", this.state.SelectionOne.ger, "DM SelectionTwo>>>", this.state.SelectionTwo.ger, "dm SelectionThree>>>>", this.state.SelectionThree.ger, "DM availableSelectionsArr>>>>", this.state.availableSelectionsArr)
+    this.setState({selectionOne: this.state.apiData[this.state.randomSelectionOne]}) // using word and random order to alocate selections to buttons
+    this.setState({selectionTwo: this.state.apiData[this.state.randomSelectionTwo]}) // same as above
+    this.setState({selectionThree: this.state.apiData[this.state.randomSelectionThree]}) // same as above
 
   }
 
