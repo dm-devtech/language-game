251f9b0094cdd982653f829778191bec7b75894f
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-06 16:25:27 +0100

    updated favicon

--- Client/language-lighthouse-react/src/components/Home.js
+++ Client/language-lighthouse-react/src/components/Home.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 class Home extends Component {
+
+  componentDidMount() {
+      document.title = "Language lighthouse";
+  }
+
   render(){
       return (
         <div>
