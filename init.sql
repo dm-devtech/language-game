edbc3a385df7578e75727d28a0a8a9f86030daef
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-04-09 16:18:18 +0100

    updated database schema

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE languagegame WITH ENCODING 'UTF8';
 CREATE TABLE german (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "ger" VARCHAR(100) NOT NULL, "wordtype" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL, "gender" VARCHAR(100) NOT NULL);
-INSERT INTO german (eng, ger, wordtype, category, gender) VALUES ('To run', 'laufen', 'verb', 'movement', 'n/a');
+INSERT INTO german (eng, ger, wordtype, category, gender, difficulty) VALUES ('To run', 'laufen', 'verb', 'movement', 'n/a', 'easy');
 
 CREATE TABLE french (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "fre" VARCHAR(100) NOT NULL, "wordtype" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL, "gender" VARCHAR(100) NOT NULL);
-INSERT INTO french (eng, fre, wordtype, category, gender) VALUES ('pan', 'la poêle', 'noun', 'kitchen', 'la');
+INSERT INTO french (eng, fre, wordtype, category, gender, difficulty) VALUES ('pan', 'la poêle', 'noun', 'kitchen', 'la', 'easy');
 
 CREATE TABLE latin (ID SERIAL PRIMARY KEY, "eng" VARCHAR(100) NOT NULL, "lat" VARCHAR(100) NOT NULL, "wordtype" VARCHAR(100) NOT NULL, "category" VARCHAR(100) NOT NULL, "gender" VARCHAR(100) NOT NULL, "difficulty" VARCHAR(100) NOT NULL, "declension" VARCHAR(100) NOT NULL);
 INSERT INTO latin (eng, lat, wordtype, category, gender, difficulty, declension) VALUES ('pan', 'cacabus', 'noun', 'kitchen', 'masculine', 'easy', '2nd');
