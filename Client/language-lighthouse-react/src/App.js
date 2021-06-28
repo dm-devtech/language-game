190dd01900eca4deb82779330099f5fd983e3ea6
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-06-28 13:29:41 +0100

    refactor: updated react routes

--- Client/language-lighthouse-react/src/App.js
+++ Client/language-lighthouse-react/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { Route, Router, Switch } from "react-router-dom";
 import './App.css';
 import GermanNouns from './components/GermanNouns.js'
 import GermanVerbs from './components/GermanVerbs.js'
@@ -10,38 +10,29 @@ import LatinVerbs from './components/LatinVerbs.js'
 import Footer from './components/Footer.js'
 import Home from './components/Home.js'
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    const App = () => (
     <div className="container">
-          <Route exact path="/">
-            <Home />
-          </Route>
-            <Route path="/game/german/nouns">
-              <GermanNouns />
-              <Footer />
-            </Route>
-              <Route path="/game/german/verbs">
-                <GermanVerbs />
-                <Footer />
-              </Route>
-          <Route path="/game/french/nouns">
-            <FrenchNouns />
-            <Footer />
-          </Route>
-          <Route path="/game/french/verbs">
-            <FrenchVerbs />
-            <Footer />
-          </Route>
-          <Route path="/game/latin/nouns">
-            <LatinNouns />
-            <Footer />
-          </Route>
-          <Route path="/game/latin/verbs">
-            <LatinVerbs />
-            <Footer />
-          </Route>
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/game/german/nouns' component={GermanNouns} />
+        <Route path='/game/german/verbs' component={GermanVerbs} />
+        <Route path='/game/french/nouns' component={FrenchNouns} />
+        <Route path='/game/french/verbs' component={FrenchVerbs} />
+        <Route path='/game/latin/nouns' component={LatinNouns} />
+        <Route path='/game/latin/verbs' component={LatinVerbs} />
+      </Switch>
+
       </div>
-  );
+    )
+    return (
+      <Switch>
+        <App/>
+      </Switch>
+    );
+  }
 }
 
 export default App;
